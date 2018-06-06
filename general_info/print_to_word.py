6d8270bfd3d67d3d49aac107654d5f9d8cdbea79
Rene Jorissen <rene@booches.nl>
2018-06-06 15:10:40 +0200

    General Test Scripts

---
+++ general_info/print_to_word.py
@@ -0,0 +1,34 @@
+from docx import Document
+from docx.shared import Inches
+
+document = Document()
+
+document.add_picture('guest_passes/logo_gsg.jpg', width=Inches(4.00))
+document.add_heading('Gast gebruiker', 0)
+
+p = document.add_paragraph('Onderstaand worden de account details voor deze week weergegeven.')
+#p.add_run('bold').bold = True
+#p.add_run(' and some ')
+#p.add_run('italic.').italic = True
+
+#document.add_heading('Heading, level 1', level=1)
+#document.add_paragraph('Intense quote', style='IntenseQuote')
+
+#document.add_paragraph(
+#    'first item in unordered list', style='ListBullet'
+#)
+#document.add_paragraph(
+#    'first item in ordered list', style='ListNumber'
+#)
+
+
+
+table = document.add_table(rows=1, cols=2)
+hdr_cells = table.rows[0].cells
+hdr_cells[0].text = 'Qty'
+hdr_cells[1].text = 'Id'
+
+
+document.add_page_break()
+
+document.save('guest_passes/{}.docx'.format(username))
---
+++ general_info/time_epoch.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+#------------------------------------------------------------------------------
+#
+# Script to "play" with time for guest accounts
+#
+#------------------------------------------------------------------------------
+
+import time
+import datetime
+
+print("Time in seconds since the epoch: %s" %time.time())
+print("Current date and time: " , datetime.datetime.now())
+print("Or like this: " ,datetime.datetime.now().strftime("%y-%m-%d-%H-%M"))
+
+print("Current year: ", datetime.date.today().strftime("%Y"))
+print("Month of year: ", datetime.date.today().strftime("%B"))
+print("Week number of the year: ", datetime.date.today().strftime("%W"))
+print("Weekday of the week: ", datetime.date.today().strftime("%w"))
+print("Day of year: ", datetime.date.today().strftime("%j"))
+print("Day of the month : ", datetime.date.today().strftime("%d"))
+print("Day of week: ", datetime.date.today().strftime("%A"))
+
+date_time = '2018-06-25 00:00:01'
+pattern = '%Y-%m-%d %H:%M:%S'
+epoch1 = int(time.mktime(time.strptime(date_time, pattern)))
+print(date_time, epoch1)
+
+date_time = '2018-07-01 23:59:59'
+pattern = '%Y-%m-%d %H:%M:%S'
+epoch2 = int(time.mktime(time.strptime(date_time, pattern)))
+print(date_time, epoch2)
+
+epoch3 = epoch2 - epoch1
+print(epoch3)
+
+
+timestamp = 1532296800
+value = datetime.datetime.fromtimestamp(timestamp)
+print(value.strftime('%Y-%m-%d %H:%M:%S'))
